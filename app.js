13b316a1f9a69d5d2f0ed0a57ca89e2b27a180e2
rishulpu1 <rishulpu1@gmail.com>
2024-12-17 14:10:53 +0530

    reading data from file based api

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
 const port = 3000;
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from server side...');
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: { tours },
+  });
 });
+// app.get('/', (req, res) => {
+//   res.status(200).send('Hello from server side...');
+// });
 app.listen(port, () => {
   console.log(`app running on ${port}`);
 });
