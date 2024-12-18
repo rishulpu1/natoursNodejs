feb46a1558f489c9436580d8308a5647101bbbac
rishulpu1 <rishulpu1@gmail.com>
2024-12-18 17:43:25 +0530

    responding to url parameters

--- app.js
+++ app.js
@@ -15,22 +15,43 @@ app.get('/api/v1/tours', (req, res) => {
     data: { tours },
   });
 });
-
+app.get('/api/v1/tours/:id', (req, res) => {
+  //console.log(req.params);
+  const tourId = req.params.id * 1;
+  const tour = tours.find((el) => el.id === tourId);
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+    return;
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
 app.post('/api/v1/tours', (req, res) => {
   //console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({id: newId}, req.body);
+  const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: "success",
-      data: {
-        tour: newTour
-      }
-    })
-  })
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
   //res.send('done');
-})
+});
 // app.get('/', (req, res) => {
 //   res.status(200).send('Hello from server side...');
 // });
