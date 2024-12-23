da7ccb38c77154d659195bce4a58520926a507ae
rishulpu1 <rishulpu1@gmail.com>
2024-12-23 18:15:40 +0530

    param middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkId = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if (val * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -15,13 +26,6 @@ exports.getTour = (req, res) => {
   //console.log(req.params);
   const tourId = req.params.id * 1;
   const tour = tours.find((el) => el.id === tourId);
-  if (!tour) {
-    res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-    return;
-  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -55,12 +59,6 @@ exports.updateTour = (req, res) => {
   });
 };
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
   res.status(204).json({
     status: 'success',
     data: null,
