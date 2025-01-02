8657556a0d723eda676bab3e46bdb18a6456dc5c
Rajneesh Kumar <rishulpu1@gmail.com>
2025-01-03 00:11:48 +0530

    tours model created

--- server.js
+++ server.js
@@ -18,7 +18,35 @@ mongoose
   .then(() => {
     console.log('DB connection successful');
   });
-
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour name must be given'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour price must be given'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'Tour to Mcleod',
+  rating: 4.8,
+  price: 100,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error', err);
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on ${port}`);
