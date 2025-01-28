9f4e054e810bc5448eda1522620348c0ee727d02
rishulpu1 <rishulpu1@gmail.com>
2025-01-28 18:42:38 +0530

    built in validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'Tour name must be given'],
       unique: true,
       trim: true,
+      maxlength: [40, 'Tour name must have less or equal than 40 characters'],
+      minlength: [10, 'Tour name must have more or equal than 10 characters']
     },
     slug: String,
     duration: {
@@ -21,10 +23,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'Tour difficulty must be given'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
