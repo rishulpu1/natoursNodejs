79792a646153e51306a55e506fbde16cb8498d6a
rishulpu1 <rishulpu1@gmail.com>
2025-02-10 11:32:23 +0530

    db id errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+}
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -8,10 +14,19 @@ const sendErrorDev = (err, res) => {
 }
 
 const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if(err.isOperational){
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('Error', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+ 
 }
 module.exports = (err, req, res, next) => {
     
@@ -20,7 +35,9 @@ module.exports = (err, req, res, next) => {
     if(process.env.NODE_ENV === 'development'){
       sendErrorDev(err, res);
       } else if(process.env.NODE_ENV === 'production'){ 
-         sendErrorProd(err, res);
+        let error = {...err};
+        if(error.name === 'CastError') error = handleCastErrorDB(error);
+        sendErrorProd(error, res);
       }
     
   }
